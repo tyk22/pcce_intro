111b27f435cc1c254b0a382c0b1e22d4f960c70d
tyk22 <tykkor22@gmail.com>
2024-12-02 16:31:21 +0900

    [kty] 프로그래머스 입문 최초 commit

---
+++ pcce_intro/src/day01/Run.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class Run {
+
+	public static void main(String[] args) {
+		
+		Solution01 s1 = new Solution01();
+		int n1 = s1.solution(2, 3);
+		System.out.println("2, 3일 때 : "+n1);
+		
+		int n2 = s1.solution(100, 2);
+		System.out.println("100, 2일때 : "+n2);
+		
+	}
+
+}
---
+++ pcce_intro/src/day01/Solution01.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Solution01 {
+	
+	 public int solution(int num1, int num2) {
+	        int answer = -1;
+	        answer = num1+num2;
+	        return answer;
+	    }
+	
+}
